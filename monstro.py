e4143d5515c38f1b6cbbec493f942da5fbde3746
Lygia Ferronato Bueno <lybueno@gmail.com>
2023-03-22 14:31:48 -0300

    2: classe Monstro

---
+++ monstro.py
@@ -0,0 +1,7 @@
+from serVivo import SerVivo
+
+class Monstro(SerVivo):
+
+    def __init__(self, pontos_de_vida, pontos_de_ataque, tipo):
+        super().__init__(pontos_de_vida, pontos_de_ataque)
+        self.tipo = tipo
